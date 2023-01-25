8fff6efa0da2b5d03da6240d5c9b3b337bbe05da
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-25 01:48:12 -0300

    Refactored src/index.js for first user route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 import express from 'express'
+import userRoute from './routes/user.route.js'
+
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.use('/user', userRoute)
+
+/* app.get('/', (req, res) => {
     res.send({
         message: 'hello world'
     })
-})
+}) */
 
 app.listen(3000, () => {
     console.log('app running')
