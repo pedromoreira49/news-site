970d03e70a3186aace418377dc8c2e0cb9a0ac9a
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-25 00:36:12 -0300

    Added src/index.js file for initial project's backend

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'hello world'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('app running')
+})
