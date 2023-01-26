508a44d8b985e65979df44438798949a3247b477
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-26 20:13:55 -0300

    Added new method for update user in service

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -5,3 +5,16 @@ export const createService = (body) => User.create(body)
 export const findAllService = () => User.find()
 
 export const findByIdService = (id) => User.findById(id)
+
+export const updateService = (
+    id,
+    name,
+    username,
+    email,
+    password,
+    avatar,
+    background
+) => User.findOneAndUpdate(
+    {_id: id}, 
+    {name, username, email, password, avatar, background}
+)
