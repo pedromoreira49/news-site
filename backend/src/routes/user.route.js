ed7b5f16f3e2f8c235925e968e6fa931b4f7adde
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-26 20:15:57 -0300

    Added patch route for update user information

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import {createUser, findAll, findById} from "../controllers/user.controller.js"
+import {createUser, findAll, findById, updateUser} from "../controllers/user.controller.js"
 
 const userRoute = Router()
 
 userRoute.post('/', createUser)
 userRoute.get('/', findAll)
 userRoute.get('/:id', findById)
+userRoute.patch('/:id', updateUser)
 
 export default userRoute
