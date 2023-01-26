3b6a61b39f80c82a1c0cf879a840b2214e71f2d3
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-26 19:19:41 -0300

    Added validation for find user by id in controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
-import {createService, findAllService} from "../services/user.service.js"
+import mongoose from "mongoose"
+import {createService, findAllService, findByIdService} from "../services/user.service.js"
 
 export const createUser = async (req, res) => {
     const {name, username, email, password, avatar, background} = req.body
@@ -39,7 +40,25 @@ export const findAll = async (req, res) => {
         })
     }
 
-    res.status(200).send({users})
+    res.status(200).send(users)
 }
 
+export const findById = async (req, res) => {
+    const id = req.params.id
 
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({
+            message: "Invalid ID"
+        })
+    }
+
+    const user = await findByIdService(id)
+
+    if(!user){
+        return res.status(400).send({
+            message: "User not found!"
+        })
+    }
+
+    res.send(user)
+}
