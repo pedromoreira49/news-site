7be27f0afc67b739a3d40e04a06983700825c02a
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-26 17:46:58 -0300

    Added const port for index js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,18 +3,13 @@ import userRoute from './routes/user.route.js'
 import connect from './database/db.js'
 
 const app = express()
+const port = 3000
 
 connect()
 app.use(express.json())
 
 app.use('/user', userRoute)
 
-/* app.get('/', (req, res) => {
-    res.send({
-        message: 'hello world'
-    })
-}) */
-
-app.listen(3000, () => {
-    console.log('app running')
+app.listen(port, () => {
+    console.log(`app running on port: ${port}`)
 })
