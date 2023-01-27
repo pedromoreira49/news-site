b484496380b9e5784b2e407395e21cf2275c328f
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-27 19:47:26 -0300

    Added middlewares in the user route get by id and patch

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import {createUser, findAll, findById, updateUser} from "../controllers/user.controller.js"
+import { validId, validUser } from "../middlewares/global.middlewares.js"
 
 const userRoute = Router()
 
 userRoute.post('/', createUser)
 userRoute.get('/', findAll)
-userRoute.get('/:id', findById)
-userRoute.patch('/:id', updateUser)
+userRoute.get('/:id', validId, validUser,findById)
+userRoute.patch('/:id', validId, validUser,updateUser)
 
 export default userRoute
