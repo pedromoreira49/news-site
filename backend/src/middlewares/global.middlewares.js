dcde6a0844e8629641087fe5dac18e744683c0af
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-27 19:46:21 -0300

    Added global middlewares in the application

---
+++ backend/src/middlewares/global.middlewares.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+import { findByIdService } from '../services/user.service.js'
+
+export const validId = (req, res, next) => {
+    const id = req.params.id
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({
+            message: "Invalid ID."
+        })
+    }
+
+    next()
+}
+
+export const validUser = async (req, res, next) => {
+    const id = req.params.id
+
+    const user = await findByIdService(id)
+
+    if(!user){
+        return res.status(400).send({
+            message: "User not found."
+        })
+    }
+
+    req.id = id
+    req.user = user
+
+    next()
+}
