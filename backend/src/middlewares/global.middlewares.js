da4339f4f40eb1c4c6f83e6810d498b24510298e
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-28 18:25:33 -0300

    Added try catch in middlewares methods

--- backend/src/middlewares/global.middlewares.js
+++ backend/src/middlewares/global.middlewares.js
@@ -2,30 +2,42 @@ import mongoose from 'mongoose'
 import { findByIdService } from '../services/user.service.js'
 
 export const validId = (req, res, next) => {
-    const id = req.params.id
+    try{
+        const id = req.params.id
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).send({
-            message: "Invalid ID."
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).send({
+                message: "Invalid ID."
+            })
+        }
+
+        next()
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
         })
     }
-
-    next()
 }
 
 export const validUser = async (req, res, next) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
 
-    const user = await findByIdService(id)
+        const user = await findByIdService(id)
 
-    if(!user){
-        return res.status(400).send({
-            message: "User not found."
+        if(!user){
+            return res.status(400).send({
+                message: "User not found."
+            })
+        }
+
+        req.id = id
+        req.user = user
+
+        next()
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
         })
     }
-
-    req.id = id
-    req.user = user
-
-    next()
 }
