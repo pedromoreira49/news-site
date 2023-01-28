d7277da24eebfdd4a9de87d1536f3b3a88207db0
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-28 19:35:15 -0300

    Modified string in database connection

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose"
-import * as dotenv from 'dotenv'
-
-dotenv.config()
 
 const connect = () => {
     console.log('Waiting connection...')
 
-    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qnf2ekc.mongodb.net/?retryWrites=true&w=majority`, {
+    mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => console.log('Database Connected: Mongodb Atlas')).catch((err) => console.log(err))
