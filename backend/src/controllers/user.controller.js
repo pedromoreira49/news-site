cff666fb0e6473a9e4ee4b81272933665e5e07c5
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-28 18:25:00 -0300

    Added try catch in controller user methods

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,75 +2,100 @@ import mongoose from "mongoose"
 import {createService, findAllService, findByIdService, updateService} from "../services/user.service.js"
 
 export const createUser = async (req, res) => {
-    const {name, username, email, password, avatar, background} = req.body
+    
+    try{
+        const {name, username, email, password, avatar, background} = req.body
 
-    if(!name || !username || !email || !password || !avatar || !background){
-        res.status(400).send({
-            message: 'Submit all fields for registration!'
-        })
-    }
-
-    const user = await createService(req.body)
-
-    if(!user){
-        return res.status(400).send({
-            message: "Error creating User"
-        })
-    }
-
-    res.status(201).send({
-        message: "User created successfully",
-        user: {
-            id: user._id,
-            name,
-            username,
-            email,
-            avatar,
-            background
+        if(!name || !username || !email || !password || !avatar || !background){
+            res.status(400).send({
+                message: 'Submit all fields for registration!'
+            })
         }
-    })
+
+        const user = await createService(req.body)
+
+        if(!user){
+            return res.status(400).send({
+                message: "Error creating User"
+            })
+        }
+
+        res.status(201).send({
+            message: "User created successfully",
+            user: {
+                id: user._id,
+                name,
+                username,
+                email,
+                avatar,
+                background
+            }
+        })
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
+        })
+    }
 }
 
 export const findAll = async (req, res) => {
-    const users = await findAllService()
+    try{
+        const users = await findAllService()
 
-    if(users.length === 0){
-        return res.status(400).send({
-            message: "There are no registered users"
+        if(users.length === 0){
+            return res.status(400).send({
+                message: "There are no registered users"
+            })
+        }
+
+        res.status(200).send(users)
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
         })
     }
-
-    res.status(200).send(users)
 }
 
 export const findById = async (req, res) => {
-    const user = req.user
+    try{
+        const user = req.user
 
-    res.send(user)
+        res.send(user)
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
+        })
+    }
 }
 
 export const updateUser = async (req, res) => {
-    const {name, username, email, password, avatar, background} = req.body
+    try{
+        const {name, username, email, password, avatar, background} = req.body
 
-    if(!name && !username && !email && !password && !avatar && !background){
-        res.status(400).send({
-            message: 'Submit at least one field for update!'
+        if(!name && !username && !email && !password && !avatar && !background){
+            res.status(400).send({
+                message: 'Submit at least one field for update!'
+            })
+        }
+
+        const {id, user} = req
+
+        await updateService(
+            id,
+            name,
+            username,
+            email,
+            password,
+            avatar,
+            background
+        )
+
+        res.send({
+            message: "User successfully updated!"
+        })
+    } catch (err) {
+        res.status(500).send({
+            message: err.message
         })
     }
-
-    const {id, user} = req
-
-    await updateService(
-        id,
-        name,
-        username,
-        email,
-        password,
-        avatar,
-        background
-    )
-
-    res.send({
-        message: "User successfully updated!"
-    })
 }
