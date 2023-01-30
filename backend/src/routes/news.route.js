a160479a7b3355a7fc49f21ac52ee1063cb0b392
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-30 00:16:55 -0300

    Refactored middlewares in news route

--- backend/src/routes/news.route.js
+++ backend/src/routes/news.route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { create, findAll } from '../controllers/news.controller.js'
+import { authMiddleware } from '../middlewares/auth.middlewares.js'
 
 const newsRoute = Router()
 
-newsRoute.post('/', create)
+newsRoute.post('/', authMiddleware, create)
 newsRoute.get('/', findAll)
 
 export default newsRoute
