bd831406082d9ca5866732803bd624637261b198
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-01-29 22:39:02 -0300

    Added news route in index file

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import connect from './database/db.js'
 
 import userRoute from './routes/user.route.js'
 import loginRoute from './routes/auth.route.js'
+import newsRoute from './routes/news.route.js'
 
 dotenv.config()
 const app = express()
@@ -14,6 +15,7 @@ app.use(express.json())
 
 app.use('/user', userRoute)
 app.use('/login', loginRoute)
+app.use('/news', newsRoute)
 
 app.listen(port, () => {
     console.log(`app running on port: ${port}`)
