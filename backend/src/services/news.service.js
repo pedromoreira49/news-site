43db57be5ddc43b1ce623947a39c28ae12e48d30
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-02-02 17:09:55 -0300

    feat: add search by title feature in news service

--- backend/src/services/news.service.js
+++ backend/src/services/news.service.js
@@ -13,3 +13,7 @@ export const countNews = () => News.countDocuments()
 export const topNewsService = () => News.findOne().sort({_id: -1}).populate("user")
 
 export const findByIdService = (id) => News.findById(id).populate("user")
+
+export const findByTitleService = (title) => News.find({
+    title: {$regex: `${title || ""}`, $options: "i"}, //opt = i (Case insensitive)
+}).sort({ _id: -1 }).populate("user")
