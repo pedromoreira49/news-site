1b12780356f1d5fba0538f31b85f9fc5b8d445f9
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-02-02 02:36:30 -0300

    feat: add get route in news to find by id news and add middleware to validate id

--- backend/src/routes/news.route.js
+++ backend/src/routes/news.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
-import { create, findAll, topNews } from '../controllers/news.controller.js'
+import { create, findAll, topNews, findById } from '../controllers/news.controller.js'
 import { authMiddleware } from '../middlewares/auth.middlewares.js'
+import { validId } from "../middlewares/global.middlewares.js"
 
 const newsRoute = Router()
 
 newsRoute.post('/', authMiddleware, create)
 newsRoute.get('/', findAll)
 newsRoute.get('/top', topNews)
+newsRoute.get('/:id', validId, findById)
 
 export default newsRoute
