57935b12727e8505a31280eafb28a0eef7f528d6
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-02-02 02:03:46 -0300

    feat: add new get route in news route for push top news

--- backend/src/routes/news.route.js
+++ backend/src/routes/news.route.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { create, findAll } from '../controllers/news.controller.js'
+import { create, findAll, topNews } from '../controllers/news.controller.js'
 import { authMiddleware } from '../middlewares/auth.middlewares.js'
 
 const newsRoute = Router()
 
 newsRoute.post('/', authMiddleware, create)
 newsRoute.get('/', findAll)
+newsRoute.get('/top', topNews)
 
 export default newsRoute
