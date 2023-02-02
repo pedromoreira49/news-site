3dc5047be5b28c3803210b76c09b0eaa7b009ead
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-02-02 18:29:46 -0300

    feat: add update service to news service

--- backend/src/services/news.service.js
+++ backend/src/services/news.service.js
@@ -19,3 +19,5 @@ export const findByTitleService = (title) => News.find({
 }).sort({ _id: -1 }).populate("user")
 
 export const byUserService = (id) => News.find({user: id}).sort({ _id: -1 }).populate("user")
+
+export const updateService = (id, title, text, banner) => News.findOneAndUpdate({ _id: id}, {title, text, banner}, {rawResult: true})
