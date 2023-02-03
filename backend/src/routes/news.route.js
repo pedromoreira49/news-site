a65c918791c94bb4a18fc82cdaa249c264e540c7
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-02-03 01:43:51 -0300

    feat: add new patch routes in news router for add and remove comments

--- backend/src/routes/news.route.js
+++ backend/src/routes/news.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { create, findAll, topNews, findById, searchByTitle, byUser, update, deleteNews, likeNews } from '../controllers/news.controller.js'
+import { create, findAll, topNews, findById, searchByTitle, byUser, update, deleteNews, likeNews, addComment, removeComment } from '../controllers/news.controller.js'
 import { authMiddleware } from '../middlewares/auth.middlewares.js'
 import { validId } from "../middlewares/global.middlewares.js"
 
@@ -15,5 +15,7 @@ newsRoute.get('/:id', authMiddleware, validId, findById)
 newsRoute.patch('/:id', authMiddleware, update)
 newsRoute.delete('/:id', authMiddleware, deleteNews)
 newsRoute.patch('/like/:id', authMiddleware, likeNews)
+newsRoute.patch('/comment/:id', authMiddleware, addComment)
+newsRoute.patch('/comment/:idNews/:idComment', authMiddleware, removeComment)
 
 export default newsRoute
